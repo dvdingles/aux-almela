299a3139e528a6c19deaa3f4d195c39b1004e4d0
dvdapogeo <dvdingles@hotmail.com>
2022-08-03 14:17:11 +0200

    no va

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,18 @@ import {Solo} from '../components/Solo'
 const Home: NextPage = () => {
   return (
     <div style={{width: '555', height: '777px', border: 'solid 2px #eee', backgroundColor: '#f5f5f5'}}>
-    <Canvas camera={{zoom: 13, position:[1, .5 , -1]}} >
+    <Canvas camera={{zoom: 3, position:[1, 4 , 0]}} >
       {/* <focus-camera> */}
       <ambientLight intensity={1.5} />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
-        <Stage contactShadow={{ blur: 3, opacity: 1}}>
+        <Stage 
+          contactShadow={{ 
+            blur: 3, 
+            opacity: 1, 
+            position: [0,0,0]}}>
           <Solo 
-            position={[0,0,0]}
+            position={[0,-5,0]}
             rotation={[0,1.5,0]}
           />
         </Stage>
