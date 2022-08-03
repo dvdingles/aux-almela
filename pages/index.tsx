9f431b51e669eaefbc2567c5b473159dde150f44
dvdapogeo <dvdingles@hotmail.com>
2022-08-03 11:55:06 +0200

    cambio la escala

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import type { NextPage } from 'next'
 import { Suspense } from 'react'
-import Solo from '../components/Solo'
+import {Solo} from '../components/Solo'
 
 
 
 const Home: NextPage = () => {
   return (
     <div style={{width: '555', height: '777px', border: 'solid 2px #eee', backgroundColor: '#f5f5f5'}}>
-    <Canvas camera={{zoom: 2, position:[1, .5 , -1]}} >
+    <Canvas camera={{zoom: 13, position:[1, .5 , -1]}} >
       {/* <focus-camera> */}
       <ambientLight intensity={.5} />
       <pointLight position={[10, 10, 10]} />
