0ff52c9ccaac801980bf59c64a7c0ae9e17ea362
dvdapogeo <dvdingles@hotmail.com>
2022-08-04 07:39:34 +0200

    ahora si

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,25 @@ import { OrbitControls, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import type { NextPage } from 'next'
 import { Suspense } from 'react'
-import {Solo} from '../components/Solo'
+// import { David } from '../components/David'
+import { Dav } from '../components/Dav'
 
 
 
 const Home: NextPage = () => {
   return (
     <div style={{width: '555', height: '777px', border: 'solid 2px #eee', backgroundColor: '#f5f5f5'}}>
-    <Canvas camera={{zoom: 3, position:[1, 4 , 0]}} >
+    <Canvas camera={{zoom: 3, position:[0, .2, .5]}} >
       {/* <focus-camera> */}
       <ambientLight intensity={1.5} />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
-        <Stage 
-          contactShadow={{ 
-            blur: 3, 
-            opacity: 1, 
-            position: [0,0,0]}}>
-          <Solo 
-            position={[0,-5,0]}
+        
+          <Dav 
+            position={[0,0,0]}
             rotation={[0,1.5,0]}
           />
-        </Stage>
+       
       </Suspense>
       <OrbitControls autoRotate />
     </Canvas>
